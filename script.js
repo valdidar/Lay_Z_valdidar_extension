8b9d910b6b013932e97861a7d125bc3117afc9e1
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-03-02 15:20:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button").addEventListener("click", function() {
-      const text = document.getElementById('textToRead').value;  
-      const speed = document.getElementById('speed').value;      
-  		const synth = window.speechSynthesis;
-      const utterance = new SpeechSynthesisUtterance(text);
-      utterance.lang = 'en-GB';
-      utterance.rate = parseInt(speed);
-      synth.speak(utterance);
-    });
+  document.getElementById("button").addEventListener("click", function() {
+    const text = document.getElementById('textToRead').value;  
+    const speed = document.getElementById('speed').value;      
+    const synth = window.speechSynthesis;
+    const chunks = text.split(/[\.,]\s/);
+    let i = 0;
+    const speakChunks = () => {
+      if (i < chunks.length) {
+        const utterance = new SpeechSynthesisUtterance(chunks[i] + '.');
+        utterance.lang = 'en-GB';
+        utterance.rate = parseInt(speed);
+        synth.speak(utterance);
+        i++;
+        utterance.onend = speakChunks;
+      }
+    };
+    speakChunks();
   });
+});
